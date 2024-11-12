7cd7f0d2b802a58053d229ed6af48c4477f5e617
amado1992 <aramirezamdo1992@gmail.com>
2024-11-12 10:56:09 -0500

    Update project

---
+++ Take-Skip/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TakeSkip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 59, 82, 70, 56, 92, 98, 85 };
+
+            foreach (int grade in grades.Take(3))
+            {
+                Console.WriteLine("TAKE: " + grade);
+            }
+
+            foreach (int grade in grades.Skip(3))
+            {
+                Console.WriteLine("SKIP: " + grade);
+            }
+
+        }
+    }
+}
